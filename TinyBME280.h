b498dcfa754c5e89039b4d343672887a11853538
David Johnson-Davies <david@technoblogy.com>
2018-12-14 11:46:14 +0000

    Version 1 - 14th December 2018

---
+++ TinyBME280.h
@@ -0,0 +1,34 @@
+/* TinyBME280 Library
+
+   David Johnson-Davies - www.technoblogy.com - 14th December 2018
+   
+   CC BY 4.0
+   Licensed under a Creative Commons Attribution 4.0 International license: 
+   http://creativecommons.org/licenses/by/4.0/
+*/
+
+#include <stdint.h>
+#include <arduino.h>
+#include <Wire.h>
+
+#ifndef TINYBME280
+#define TINYBME280
+
+/* Function declarations */
+
+// Sets Normal mode, no upsampling, and reads the chip calibrations
+void BME280setup ();
+
+// Temperature in DegC, resolution is 0.01 DegC
+// Output value of “5123” equals 51.23 DegC
+int32_t BME280temperature();
+
+// Pressure in Pa as unsigned 32 bit integer
+// Output value of “96386” equals 96386 Pa = 963.86 hPa
+uint32_t BME280pressure();
+
+// Humidity in %RH, resolution is 0.01%RH
+// Output value of “4653” represents 46.53 %RH
+uint32_t BME280humidity ();
+
+#endif
